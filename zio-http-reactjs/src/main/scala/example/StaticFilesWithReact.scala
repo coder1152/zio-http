a22444b53b7f063222b43d704877b60a3c7f82ec
coder1152 <176615098+coder1152@users.noreply.github.com>
2024-11-19 14:27:47 +0500

    resolve scalafmt

--- zio-http-reactjs/src/main/scala/example/StaticFilesWithReact.scala
+++ zio-http-reactjs/src/main/scala/example/StaticFilesWithReact.scala
@@ -11,7 +11,7 @@ object StaticFilesWithReact extends ZIOAppDefault {
    * "/static". For paths other than the resources directory, see
    * [[zio.http.Middleware.serveDirectory]].
    */
-  val routes = Routes.empty @@ Middleware.serveResources(Path.empty / "build","build")
+  val routes = Routes.empty @@ Middleware.serveResources(Path.empty / "build", "build")
 
   override def run = Server.serve(routes).provide(Server.defaultWithPort(8090))
 }
